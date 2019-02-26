b1c812b9c41d77d5550fc6e35bbf049b18537656
vinhnd <nguyenducvinh09@gmail.com>
2019-02-26 18:22:36 +0700

    view user

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 var bodyParser = require('body-parser');
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
+const shortid = require('shortid');
+
 
 const adapter = new FileSync('db.json')
 const db = low(adapter)
@@ -45,7 +47,15 @@ app.get('/users/search', (req,res) => {
 app.get('/users/create', (req, res) => {
 	res.render('users/create')
 });
+app.get('/users/:id', (req, res) => {
+	let id = req.params.id;
+	let user =db.get('users').find({id: id}).value();
+	res.render('users/view', {
+		user : user
+	})
+});
 app.post('/users/create', (req, res) =>{
+	req.body.id = shortid.generate(),
 	db.get('users').push(req.body).write();
 	res.redirect('/users')
 });
