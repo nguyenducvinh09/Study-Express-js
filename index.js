5aad27701c9b4ee119a81c82386471e33316ecbb
vinhnd <nguyenducvinh09@gmail.com>
2019-02-26 11:45:37 +0700

    Query parameters

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+ users =[
+			{id: 1, name: "Vinh"},
+			{id: 2, name: 'Trang'}
+		];
+
 app.get('/', (req,res) => 
 	res.render('index', {
 		name: "with Coders.tokyo"
@@ -13,14 +18,20 @@ app.get('/', (req,res) =>
 );
 app.get('/users', (req,res) => 
 	res.render('./users/index', {
-		users: [
-			{id: 1, name: "Vinh"},
-			{id: 2, name: 'Trang'}
-		]
+		users: users
 	})
 );
 
-app.get('/users', (req,res) => 
-	res.send('User list :')
+app.get('/users/search', (req,res) => {
+	let q = req.query.q.toUpperCase();
+	var matchedUser = users.filter(user =>
+		user.name.toUpperCase().indexOf(q) !== -1
+	);
+	res.render('users/index', {
+		users: matchedUser,
+		paramValue: req.query.q
+	})
+}
 );
+
 app.listen(port,() => console.log('Example app listening on port ${port}'));
