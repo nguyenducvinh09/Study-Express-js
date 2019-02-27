32b7a1f576733cf53050f3dafd03387458ba7806
vinhnd <nguyenducvinh09@gmail.com>
2019-02-27 18:07:15 +0700

    static-file

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.set('views', './views');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+app.use(express.static('public'));
+
 app.get('/', (req,res) => 
 	res.render('index', {
 		name: "with Coders.tokyo"
