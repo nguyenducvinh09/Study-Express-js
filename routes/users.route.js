df098f8c26ba999c1af1b3f9d7c46f4a53963621
vinhnd <nguyenducvinh09@gmail.com>
2019-03-24 16:42:29 +0700

    File upload
    Signed-off-by: vinhnd <nguyenducvinh09@gmail.com>

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,5 +1,7 @@
 var express = require('express')
+var multer  = require('multer')
 
+var upload = multer({ dest: './public/uploads/' })
 const controller = require('../controllers/user.controller');
 const validate = require('../validate/user.validate');
 // const authMiddleware = require('../middlewares/auth.Middleware');
@@ -15,6 +17,9 @@ router.get('/create', controller.create);
 router.get('/:id', controller.getId);
 
 
-router.post('/create', validate.postCreate, controller.createPost);
+router.post('/create',
+ upload.single('avatar'),
+  validate.postCreate,
+   controller.createPost);
 
 module.exports = router;
